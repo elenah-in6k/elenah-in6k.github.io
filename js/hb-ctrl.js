1922eb8cb6ba9c9d09131a299d29fc1f56237e43
elenah <elenah@interlink-ua.com>
2016-09-13 21:25:38 +0300

    add tim

--- js/hb-ctrl.js
+++ js/hb-ctrl.js
@@ -3,6 +3,7 @@ angular.module('myApp')
     .controller('hbCtrl', function ($scope, $rootScope) {
     $rootScope.nastia = "Сируха";
         $rootScope.audioScore = 0;
+        $rootScope.tim = "";
 
         $rootScope.login = function (username, password) {
             if (username == "сируха" && password=="korona18")
@@ -18,17 +19,31 @@ angular.module('myApp')
         $rootScope.audioTrue = function() {
             $rootScope.audioScore = $rootScope.audioScore +1 ;
             alert("Всьо ок :))))") ;
-        }
+        };
 
         $rootScope.audioFalse = function() {
             alert("Не вірно, спробуй ще :))))") ;
-        }
+        };
 
         $rootScope.audio = function() {
-            var value = "";
             if($rootScope.audioScore > 5)
             alert("Ти гарно знаєш улюблені пісні :))))") ;
             else
             alert("стид-позор ти не пам'ятаєш пісень");
-        }
+        };
+
+        $rootScope.timYes = function() {
+            $rootScope.tim = " ТИ ВИГРАЛА СУПЕР-ГРУ))))";
+            $rootScope.tim1 = " Твій приз лежить під подушкою в кімнаті)))";
+        };
+
+        $rootScope.timNo = function() {
+            $rootScope.tim = " Нажаль ти програла супер-гру(((((";
+            $rootScope.tim1 = "  Приходь наступного року ;)ЬЬЬ";
+        };
+
+        $rootScope.timHelp = function() {
+            $rootScope.tim = "";
+            $rootScope.tim1 = " Загугли і не позорь мене)))";
+        };
 });
