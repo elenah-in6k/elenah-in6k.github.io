ba192d2a18b966a0fdca42036e1bca17be2523a9
elenah <elenah@interlink-ua.com>
2016-09-13 19:43:41 +0300

    music

--- js/app.js
+++ js/app.js
@@ -3,9 +3,45 @@ angular.module("myApp", ["ngRoute"])
     .config( function ($routeProvider) {
         $routeProvider
             .when("/", {
-                templateUrl : "login.html"
+                templateUrl : "view/login.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/cart", {
+                templateUrl : "view/cart.html"
+            })
+            .when("/ballon", {
+                templateUrl : "view/ballon.html"
+            })
+            .when("/music", {
+                templateUrl : "view/music.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
+            })
+            .when("/1", {
+                templateUrl : "view/sherlok.html"
             });
-            // .when("/1", {
-            //     templateUrl : "login.html"
-            // })
     });
--- js/hb-ctrl.js
+++ js/hb-ctrl.js
@@ -2,4 +2,15 @@
 angular.module('myApp')
     .controller('hbCtrl', function ($scope, $rootScope) {
     $rootScope.nastia = "Сируха";
+
+        $rootScope.login = function (username, password) {
+            if (username == "сируха" && password=="korona18")
+            alert("ok");
+            else  if (username == "сируха" && password !="korona18")
+                alert("Не вірний пароль");
+            else  if (username != "сируха" && password =="korona18")
+                alert("Не вірний логін");
+            else
+                alert("Не вірний пароль і логін");
+        }
 });
